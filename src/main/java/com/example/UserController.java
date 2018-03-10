6c3e7ca189c52b67a7070b62ccd4c067110f8525
theroar <1377202477@qq.com>
2018-03-10 13:23:01 +0800

    second commit

--- src/main/java/com/example/UserController.java
+++ src/main/java/com/example/UserController.java
@@ -8,6 +8,9 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RequestParam;
 
+/**
+ * 主要controller代码
+ */
 @Controller
 public class UserController {
 
