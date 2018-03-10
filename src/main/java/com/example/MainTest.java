02034a794721489c8a1bb6311aa2aec7af05dc96
theroar <1377202477@qq.com>
2018-03-10 15:35:42 +0800

    提交修改

--- src/main/java/com/example/MainTest.java
+++ src/main/java/com/example/MainTest.java
@@ -5,9 +5,14 @@ import org.springframework.context.support.FileSystemXmlApplicationContext;
 
 public class MainTest {
 
+    /**
+     * 进行rebase测试
+     * @param args
+     */
     public static void main(String args[]) {
         ApplicationContext ctx = new FileSystemXmlApplicationContext("classpath:spring/spring-mvc.xml");
         TestInterface test1 = (TestInterface)ctx.getBean("proxyFactory");
         test1.getReflectField();
+
     }
 }
